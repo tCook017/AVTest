4b0d21b70a7a7bfbbf8fe2239adc78cc31dbdace
Tyler Cook <cookts@uci.edu>
2021-03-08 13:24:12 -0800

    Initial SD data logging and TODOs.

--- AV_Teensy3.6/AV_Teensy3.6.ino
+++ AV_Teensy3.6/AV_Teensy3.6.ino
@@ -2,14 +2,24 @@
 #include <Adafruit_FXAS21002C.h> // gyroscope
 #include <Adafruit_FXOS8700.h>   // accelerometer and magnetometer
 #include <Adafruit_BMP3XX.h>     // barometric pressure sensor
+#include <SD.h>
 #include <Wire.h>
 
 #define SEALEVELPRESSURE_HPA (1013.2)
+
+/* TODOs:
+ *  Create version without Serial prints before flight (keep Serial1)
+ *  Reduce/remove delays
+ */
+
+const int SD_SELECT = BUILTIN_SDCARD;
 
 // The FXAS21002C and the FXOS8700 constitute the NXP 9-DOF board
 Adafruit_FXAS21002C gyro = Adafruit_FXAS21002C(0x0021002C);
 Adafruit_FXOS8700 accel = Adafruit_FXOS8700(0x8700A, 0x8700B);
 Adafruit_BMP3XX bmp = Adafruit_BMP3XX();
+
+File telemFile;
 
 void setup() {
   Serial.begin(9600);
@@ -35,6 +45,10 @@ void setup() {
       Serial.println("Error detecting BMP388");
       initialized = false;
     }
+    if (!SD.begin(SD_SELECT)) {
+      Serial.println("Error detectig SD card");
+      initialized = false;
+    }
   }
 
   // Setup Barometer (Bmp388)
@@ -48,61 +62,90 @@ void setup() {
   Serial1.setRX(0);
   Serial1.setTX(1);
   Serial1.begin(9600);
+
+  telemFile = SD.open("telemFile.csv", FILE_WRITE);
+  telemFile.close();
 }
 
 void loop() {
   sensors_event_t gyro_event, accel_event;
   gyro.getEvent(&gyro_event);
   accel.getEvent(&accel_event, NULL);
+  
+  String newTelem = "";
 
-  Serial.print(millis());
+  // Read time
+  unsigned long timestamp = millis();
+  Serial.print(timestamp);
   Serial.print(" - ");
+  
+//  Serial1.print(timestamp);
+//  Serial1.print(',');
+  newTelem += String(timestamp) + ',';
+
+  // Read gyroscope
   Serial.print(gyro_event.gyro.x);
   Serial.print(", ");
   Serial.print(gyro_event.gyro.y);
   Serial.print(", ");
   Serial.println(gyro_event.gyro.z);
 
-  Serial1.print(millis());
-  Serial1.print(',');
-  Serial1.print(gyro_event.gyro.x);
-  Serial1.print(',');
-  Serial1.print(gyro_event.gyro.y);
-  Serial1.print(',');
-  Serial1.print(gyro_event.gyro.z);
-  Serial1.print("\n");
-
-  Serial.print(" - ");  // read accel {m/s^2}
+//  Serial1.print(gyro_event.gyro.x);
+//  Serial1.print(',');
+//  Serial1.print(gyro_event.gyro.y);
+//  Serial1.print(',');
+//  Serial1.print(gyro_event.gyro.z);
+  newTelem += String(gyro_event.gyro.x) + ',';
+  newTelem += String(gyro_event.gyro.y) + ',';
+  newTelem += String(gyro_event.gyro.z);
+  
+  // Read accelerometer [m/s^2]
+  Serial.print(" - ");
   Serial.print(accel_event.acceleration.x);
   Serial.print(", ");
   Serial.print(accel_event.acceleration.y);
   Serial.print(", ");
   Serial.println(accel_event.acceleration.z);
 
-  Serial1.print(',');
-  Serial1.print(accel_event.acceleration.x);
-  Serial1.print(',');
-  Serial1.print(accel_event.acceleration.y);
-  Serial1.print(',');
-  Serial1.print(accel_event.acceleration.z);
+//  Serial1.print(',');
+//  Serial1.print(accel_event.acceleration.x);
+//  Serial1.print(',');
+//  Serial1.print(accel_event.acceleration.y);
+//  Serial1.print(',');
+//  Serial1.print(accel_event.acceleration.z);
+  newTelem += ',';
+  newTelem += String(accel_event.acceleration.x) + ',';
+  newTelem += String(accel_event.acceleration.y) + ',';
+  newTelem += String(accel_event.acceleration.z);
 
-  Serial.print(" - ");  // read tempature [C]
-  Serial.println(bmp.temperature);  
-
-  Serial1.print(',');
-  Serial1.print(bmp.temperature);
-
-  Serial.print(" - ");  // read pressure [hPa]
-  Serial.println(bmp.pressure / 100);  
-
-  Serial1.print(',');
-  Serial1.print(bmp.pressure);
-
-  Serial.print(" - ");  // read altitude [m]
+  // Read bmp tempature [C], pressure [hPa], and altitude [m]
+  Serial.print(" - ");
+  Serial.print(bmp.temperature);  
+  Serial.print(", ");
+  Serial.print(bmp.pressure / 100);
+  Serial.print(", ");
   Serial.println(bmp.readAltitude(SEALEVELPRESSURE_HPA));  
 
-  Serial1.print(',');
-  Serial1.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+//  Serial1.print(',');
+//  Serial1.print(bmp.temperature);
+//  Serial1.print(',');
+//  Serial1.print(bmp.pressure); 
+//  Serial1.print(',');
+//  Serial1.println(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  newTelem += ',';
+  newTelem += String(bmp.temperature) + ',';
+  newTelem += String(bmp.pressure) + ',';
+  newTelem += String(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+
+  telemFile = SD.open("telemFile.csv", FILE_WRITE);
+  if (telemFile != NULL) {
+    telemFile.println(newTelem);
+    dataFile.close();
+    Serial.println("Wrote to SD");
+  }
+  else {
+    Serial.println("Error writing to SD");
+  }
 
   delay(1000);
 }
